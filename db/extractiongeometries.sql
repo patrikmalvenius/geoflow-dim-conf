02abc3bce378a98e39ad95ce8eb0adaf5b2ea840
Patrik Malvenius <patrik.malvenius@cdg64.fr>
2024-01-26 10:54:00 +0100

    modify sql for geomextraction

--- db/extractiongeometries.sql
+++ db/extractiongeometries.sql
@@ -18,7 +18,7 @@ select co.object_id, ST_GeomFromText('POLYGONZ(('||replace(
 						), ',') ::text, ',', ''), '][', ','), '[', ''), ']', '')||'))',2154)  as lod22 from cjdb.city_object co, arra where type = 'Building' and co.object_id = arra.object_id group by co.object_id, arra.arr 
 						
 						
---récuperer la géométrie de bâtiment entier, update colonne geombuilding					
+--récuperer la géométrie de bâtiment entier, update colonne geombuilding -- par contre je crois que c'est possible que j'ai raté des parties des géometries ici :(					
 update city_object set geombuilding = (with arra as 	(select	object_id,				array(
 							select jsonb_array_elements_text(
 								jsonb_array_elements(
@@ -34,7 +34,7 @@ select  co.object_id as id, ST_GeomFromText('POLYGONZ(('||replace(
 		array_to_string(
 					array_append(
 		arr, arr[1]
-						), ',') ::text, ',', ''), '][', ','), '[', ''), ']', '')||'))',2154)  as lod22 from cjdb.city_object co, arra where type = 'Building' and co.object_id = arra.object_id group by co.object_id, arra.arr ) select lod22 from geom where geom.id = object_id and type = 'Building' )
+						), ',') ::text, ',', ''), '][', ','), '[', ''), ']', '')||'))',2154)  as lod22 from cjdb.city_object co, arra where type = 'Building' and co.object_id = arra.object_id group by co.object_id, arra.arr ) select lod22 from geom where geom.id = object_id  )where type = 'Building'
 						
 --récuperer la géométrie de partie de bâtiment
 
@@ -52,5 +52,5 @@ update city_object set geombuilding = (with bomb as (select object_id,
 	from cjdb.city_object co where type = 'BuildingPart'), dynamite as(
 							
 select bomb.object_id, bomb.exploded[0]||jsonb_build_array(bomb.exploded[0][0]) as magic from bomb), geom as
-(select object_id,   ST_GeomFromText('MULTIPOLYGONZ('||replace(replace(replace(replace(replace(array_to_string(array_agg( magic), ':'), ',',''), ']]:[[', ')),(('), '] [', ','), '[[', '(('), ']]','))')||')',2154) as lod22 from dynamite group by object_id) select lod22 from  geom where  city_object.object_id = geom.object_id );
+(select object_id,   ST_GeomFromText('MULTIPOLYGONZ('||replace(replace(replace(replace(replace(array_to_string(array_agg( magic), ':'), ',',''), ']]:[[', ')),(('), '] [', ','), '[[', '(('), ']]','))')||')',2154) as lod22 from dynamite group by object_id) select lod22 from  geom where  city_object.object_id = geom.object_id  )where type = 'BuildingPart';
 						
